f480ecd69b5aefd1940433f6d41c233885432d43
ghaniafridi48 <pshfscmm2000548@gmail.com>
2026-06-24 12:34:33 +0500

    fix(repeatString): implement string repetition logic and enable all tests

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function (string , num) {
+    let numTimesStr = '';
+    if (num < 0) 
+        return 'ERROR';
+    else 
+        for (let index = 0; index < num; index++) {
+            numTimesStr = numTimesStr.concat(string); 
+        }
+    return numTimesStr;
+    
 };
 
 // Do not edit below this line
