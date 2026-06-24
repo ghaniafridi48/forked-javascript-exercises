c0adec24ac725b4bffd97fe1b5cf9d05a7f27c70
ghaniafridi48 <pshfscmm2000548@gmail.com>
2026-06-24 12:53:35 +0500

    implement string reversal logic and enable all tests

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let revString = '' ; 
+    for (let index = string.length-1; index >= 0 ; index--) {
+        revString += string[index];  
+    }
+    return revString;
 };
 
 // Do not edit below this line
