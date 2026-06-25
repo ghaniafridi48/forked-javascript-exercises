0e6d3ce54fcad0532bacf3dcc3ce68e37484f63b
ghaniafridi48 <pshfscmm2000548@gmail.com>
2026-06-25 18:12:29 +0500

    (sumAll): implement sum logic and enable all tests

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = function(firstNum, secondNum) {
 
+    if (firstNum < 0 || secondNum < 0 ) 
+        return 'ERROR';
+    if (!Number.isInteger(firstNum) || !Number.isInteger(secondNum)) return "ERROR";
+    let sum = 0;
+
+    if(firstNum < secondNum){
+        
+        for (let index = firstNum; index <= secondNum; index++) {
+            sum += index; 
+        }
+        return sum;
+    }
+    else {
+        
+        for (let index = secondNum; index <= firstNum; index++) {
+            sum += index; 
+        }
+        return sum;
+    }
 };
 
 // Do not edit below this line
