690488495d64a1bf89b55cc1415598d57be6e092
ghaniafridi48 <pshfscmm2000548@gmail.com>
2026-06-25 19:06:39 +0500

    implement temperature conversion functions and enable all tests

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,20 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  
+  temp = (temp - 32) * (5/9
+  );
+  if (temp % 2 !== 0)
+    return parseFloat(temp.toFixed(1));
+  else
+    return temp;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  
+  temp = (temp * 9/5 + 32);
+  if (temp % 2 !== 0)
+    return parseFloat(temp.toFixed(1));
+  else
+    return temp;
 };
 
 // Do not edit below this line
