a2882d19666dbf8a9261a8b52ae3f08d605fcdbf
ghaniafridi48 <pshfscmm2000548@gmail.com>
2026-07-11 18:29:54 +0500

    implement calculator functions and enable all tests

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function(firstNumber,secondNumber) {
+	return firstNumber + secondNumber ;
 };
 
-const subtract = function() {
-	
+const subtract = function(firstNumber,secondNumber) {
+	return firstNumber - secondNumber ;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce((result , num) => result + num,0)
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((result,num) => result * num ,1)
 };
 
-const power = function() {
-	
+const power = function(base,exponent) {
+	return base ** exponent;
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+	 let factorial = 1;
+  for(let i=1 ; i <= n ; i++){
+  factorial *= i ;
+  }
+  return factorial;
 };
 
 // Do not edit below this line
